61bf3f1d25097ce1ba86833bb6cf70d94cafb521
Carlos Arturo Torres <carlosat95@gmail.com>
2024-04-29 12:04:04 -0500

    new apiUrl

--- api.js
+++ api.js
@@ -1,7 +1,8 @@
-let apiUrl = "http://localhost:8000/"
+//let apiUrl = "http://localhost:8000/"
+let apiUrl = "https://minga-back-58at.onrender.com/"
 
-if (process.env.NODE_ENV === "production") {
+/* if (process.env.NODE_ENV === "production") {
     apiUrl = import.meta.env.VITE_API
-}
+} */
 
 export default apiUrl
